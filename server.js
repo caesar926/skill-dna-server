c2751cdad634c99bc0dd02022cbd807f702fe4b6
caesar926 <caesarexorvus15@gmail.com>
2026-09-15 14:52:37 +0100

    add console.log

--- server.js
+++ server.js
@@ -310,6 +310,7 @@ app.get('/api/profile/claim', async (req, res) => {
   },
   fetched_at: new Date().toISOString(),
 }
+console.log(languages)
 
   const { data: saved, error: saveError } = await supabase
     .from('profiles')
